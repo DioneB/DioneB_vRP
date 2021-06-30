55a88350fcf1e20f12fbd3e48aada33a7fb14da3
Dione Batista <dione1993@hotmail.com>
2021-06-30 18:46:28 -0300

    Clear Cloackroom

--- resources/[vrp]/vrp/modules/cloakroom.lua
+++ resources/[vrp]/vrp/modules/cloakroom.lua
@@ -1,31 +1,23 @@
 function vRP.save_idle_custom(player,custom)
 	local r_idle = {}
 	local user_id = vRP.getUserId(player)
-	if user_id then
-		local data = vRP.getUserDataTable(user_id)
-		if data then
-			if data.cloakroom_idle == nil then
-				data.cloakroom_idle = custom
-			end
-
-			for k,v in pairs(data.cloakroom_idle) do
-				r_idle[k] = v
-			end
-		end
+	if not user_id then return r_idle end 
+	local data = vRP.getUserDataTable(user_id)
+	if not data then return end
+	if data.cloakroom_idle == nil then
+		data.cloakroom_idle = custom
 	end
-	return r_idle
+	for k,v in pairs(data.cloakroom_idle) do
+		r_idle[k] = v
+	end
 end
 
--- remove the player uniform (cloakroom)
 function vRP.removeCloak(player)
   local user_id = vRP.getUserId(player)
-  if user_id then
-    local data = vRP.getUserDataTable(user_id)
-    if data then
-      if data.cloakroom_idle ~= nil then -- consume cloakroom idle
-        vRPclient._setCustomization(player,data.cloakroom_idle)
-        data.cloakroom_idle = nil
-      end
-    end
-  end
+  if not user_id then return end
+	local data = vRP.getUserDataTable(user_id)
+	if not data then return end
+	if not data.cloakroom_idle then return end
+	vRPclient._setCustomization(player,data.cloakroom_idle)
+	data.cloakroom_idle = nil
 end
