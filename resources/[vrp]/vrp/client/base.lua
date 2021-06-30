77254f4ba6011064fb8b741dfde29e8303e5f605
Dione Batista <dione1993@hotmail.com>
2021-06-30 19:40:33 -0300

    Clear Base Client

--- resources/[vrp]/vrp/client/base.lua
+++ resources/[vrp]/vrp/client/base.lua
@@ -1,5 +1,4 @@
 cfg = module("cfg/client")
-
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
 local Tools = module("vrp", "lib/Tools")
@@ -7,7 +6,6 @@ local Tools = module("vrp", "lib/Tools")
 tvRP = {}
 Tunnel.bindInterface("vRP", tvRP)
 vRPserver = Tunnel.getInterface("vRP")
-
 Proxy.addInterface("vRP",tvRP)
 
 function onClientMapStart(...)
@@ -16,7 +14,6 @@ function onClientMapStart(...)
   exports.spawnmanager:setAutoSpawn(true)
   exports.spawnmanager:forceRespawn()
 end
-
 AddEventHandler('onClientMapStart', onClientMapStart)
 
 function tvRP.teleport(x,y,z)
@@ -32,20 +29,17 @@ function tvRP.clearWeapons()
   RemoveAllPedWeapons(PlayerPedId(),true)
 end
 
--- return x,y,z,h
 function tvRP.getPosition()
 	local x,y,z = table.unpack(GetEntityCoords(PlayerPedId(),true))
 	local heading = GetEntityHeading(PlayerPedId())
 	return x,y,z,heading
 end
 
--- return false if in exterior, true if inside a building
 function tvRP.isInside()
   local x,y,z = tvRP.getPosition()
   return not (GetInteriorAtCoords(x,y,z) == 0)
 end
 
--- return vx,vy,vz
 function tvRP.getSpeed()
   local vx,vy,vz = table.unpack(GetEntityVelocity(PlayerPedId()))
   return math.sqrt(vx*vx+vy*vy+vz*vz)
@@ -54,19 +48,15 @@ end
 function tvRP.getCamDirection()
   local heading = GetGameplayCamRelativeHeading()+GetEntityHeading(PlayerPedId())
   local pitch = GetGameplayCamRelativePitch()
-
   local x = -math.sin(heading*math.pi/180.0)
   local y = math.cos(heading*math.pi/180.0)
   local z = math.sin(pitch*math.pi/180.0)
-
-  -- normalize
   local len = math.sqrt(x*x+y*y+z*z)
   if len ~= 0 then
     x = x/len
     y = y/len
     z = z/len
   end
-
   return x,y,z
 end
 
@@ -76,137 +66,97 @@ function tvRP.notify(msg)
   DrawNotification(true, false)
 end
 
--- SCREEN
-
--- play a screen effect
--- name, see https://wiki.fivem.net/wiki/Screen_Effects
--- duration: in seconds, if -1, will play until stopScreenEffect is called
 function tvRP.playScreenEffect(name, duration)
-  if duration < 0 then -- loop
+  if duration >= 0 then
     StartScreenEffect(name, 0, true)
-  else
-    StartScreenEffect(name, 0, true)
-
-    Citizen.CreateThread(function() -- force stop the screen effect after duration+1 seconds
+    Citizen.CreateThread(function()
       Citizen.Wait(math.floor((duration+1)*1000))
       StopScreenEffect(name)
     end)
-  end
+  return end 
+  StartScreenEffect(name, 0, true)
 end
 
--- stop a screen effect
--- name, see https://wiki.fivem.net/wiki/Screen_Effects
 function tvRP.stopScreenEffect(name)
   StopScreenEffect(name)
 end
 
--- ANIM
-
--- animations dict and names: http://docs.ragepluginhook.net/html/62951c37-a440-478c-b389-c471230ddfc5.htm
-
 local anims = {}
 local anim_ids = Tools.newIDGenerator()
-
--- play animation (new version)
--- upper: true, only upper body, false, full animation
--- seq: list of animations as {dict,anim_name,loops} (loops is the number of loops, default 1) or a task def (properties: task, play_exit)
--- looping: if true, will infinitely loop the first element of the sequence until stopAnim is called
 function tvRP.playAnim(upper, seq, looping)
-  if seq.task then -- is a task (cf https://github.com/ImagicTheCat/vRP/pull/118)
+  if seq.task then
     tvRP.stopAnim(true)
-
     local ped = PlayerPedId()
-    if seq.task == "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER" then -- special case, sit in a chair
+    if seq.task == "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER" then
       local x,y,z = tvRP.getPosition()
       TaskStartScenarioAtPosition(ped, seq.task, x, y, z-1, GetEntityHeading(ped), 0, 0, false)
-    else
-      TaskStartScenarioInPlace(ped, seq.task, 0, not seq.play_exit)
-    end
-  else -- a regular animation sequence
-    tvRP.stopAnim(upper)
-
-    local flags = 0
-    if upper then flags = flags+48 end
-    if looping then flags = flags+1 end
-
-    Citizen.CreateThread(function()
-      -- prepare unique id to stop sequence when needed
-      local id = anim_ids:gen()
-      anims[id] = true
-
-      for k,v in pairs(seq) do
-        local dict = v[1]
-        local name = v[2]
-        local loops = v[3] or 1
-
-        for i=1,loops do
-          if anims[id] then -- check animation working
-            local first = (k == 1 and i == 1)
-            local last = (k == #seq and i == loops)
-
-            -- request anim dict
+    return end
+    TaskStartScenarioInPlace(ped, seq.task, 0, not seq.play_exit)
+  return end
+  tvRP.stopAnim(upper)
+  local flags = 0
+  if upper then flags = flags+48 end
+  if looping then flags = flags+1 end
+  Citizen.CreateThread(function()
+    local id = anim_ids:gen()
+    anims[id] = true
+    for k,v in pairs(seq) do
+      local dict = v[1]
+      local name = v[2]
+      local loops = v[3] or 1
+      for i=1,loops do
+        if anims[id] then
+          local first = (k == 1 and i == 1)
+          local last = (k == #seq and i == loops)
+          RequestAnimDict(dict)
+          local i = 0
+          while not HasAnimDictLoaded(dict) and i < 1000 do -- max time, 10 seconds
+            Citizen.Wait(10)
             RequestAnimDict(dict)
-            local i = 0
-            while not HasAnimDictLoaded(dict) and i < 1000 do -- max time, 10 seconds
-              Citizen.Wait(10)
-              RequestAnimDict(dict)
-              i = i+1
-            end
-
-            -- play anim
-            if HasAnimDictLoaded(dict) and anims[id] then
-              local inspeed = 8.0001
-              local outspeed = -8.0001
-              if not first then inspeed = 2.0001 end
-              if not last then outspeed = 2.0001 end
-
-              TaskPlayAnim(PlayerPedId(),dict,name,inspeed,outspeed,-1,flags,0,0,0,0)
-            end
-
+            i = i+1
+          end
+          if HasAnimDictLoaded(dict) and anims[id] then
+            local inspeed = 8.0001
+            local outspeed = -8.0001
+            if not first then inspeed = 2.0001 end
+            if not last then outspeed = 2.0001 end
+            TaskPlayAnim(PlayerPedId(),dict,name,inspeed,outspeed,-1,flags,0,0,0,0)
+          end
+          Citizen.Wait(0)
+          while GetEntityAnimCurrentTime(PlayerPedId(),dict,name) <= 0.95 and IsEntityPlayingAnim(PlayerPedId(),dict,name,3) and anims[id] do
             Citizen.Wait(0)
-            while GetEntityAnimCurrentTime(PlayerPedId(),dict,name) <= 0.95 and IsEntityPlayingAnim(PlayerPedId(),dict,name,3) and anims[id] do
-              Citizen.Wait(0)
-            end
           end
         end
       end
-
-      -- free id
-      anim_ids:free(id)
-      anims[id] = nil
-    end)
-  end
+    end
+    anim_ids:free(id)
+    anims[id] = nil
+  end)
 end
 
--- stop animation (new version)
--- upper: true, stop the upper animation, false, stop full animations
 function tvRP.stopAnim(upper)
-  anims = {} -- stop all sequences
+  anims = {}
   if upper then
     ClearPedSecondaryTask(PlayerPedId())
-  else
-    ClearPedTasks(PlayerPedId())
-  end
+  return end
+  ClearPedTasks(PlayerPedId())
 end
 
--- RAGDOLL
 local ragdoll = false
-
--- set player ragdoll flag (true or false)
-function tvRP.setRagdoll(flag)
-  ragdoll = flag
-end
-
--- ragdoll thread
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(10)
-    if ragdoll then
+local function RagdollThread()
+  Citizen.CreateThread(function()
+    while true do
+      Citizen.Wait(10)
+      if not ragdoll then return end
       SetPedToRagdoll(PlayerPedId(), 1000, 1000, 0, 0, 0, 0)
     end
-  end
-end)
+  end)
+end
 
+function tvRP.setRagdoll(flag)
+  ragdoll = flag
+  RagdollThread()
+end
 
 AddEventHandler("playerSpawned",function()
   TriggerServerEvent("vRPcli:playerSpawned")
@@ -225,7 +175,6 @@ Citizen.CreateThread(function()
 		Citizen.Wait(1)
 		if NetworkIsSessionStarted() then
 			TriggerServerEvent("Queue:playerActivated")
-			return
-		end
+		return end
 	end
 end)
