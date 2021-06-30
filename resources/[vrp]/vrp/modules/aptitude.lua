393fd34450c0f5bd7ee3a3bbc17010584c83ced6
Dione Batista <dione1993@hotmail.com>
2021-06-30 18:44:32 -0300

    Clear Apitude Module

--- resources/[vrp]/vrp/modules/aptitude.lua
+++ resources/[vrp]/vrp/modules/aptitude.lua
@@ -1,8 +1,4 @@
-
--- define aptitude system (aka. education, skill system)
-
 local cfg = module("cfg/aptitudes")
-
 -- exp notes:
 -- levels are defined by the amount of xp
 -- with a step of 5: 5|15|30|50|75
@@ -10,9 +6,7 @@ local cfg = module("cfg/aptitudes")
 -- level for a specific exp amount, lvl = (sqrt(1+8*exp/step)-1)/2
 
 local exp_step = 5
-
 local gaptitudes = {}
-
 function vRP.defAptitudeGroup(group, title)
   gaptitudes[group] = {_title = title}
 end
@@ -28,76 +22,58 @@ end
 function vRP.getAptitudeDefinition(group, aptitude)
   local vgroup = gaptitudes[group]
   if vgroup ~= nil and aptitude ~= "_title" then
-    return vgroup[aptitude]
-  else
-    return nil
+    return vgroup[aptitude]   
   end
+  return nil
 end
 
 function vRP.getAptitudeGroupTitle(group)
   if gaptitudes[group] ~= nil then
     return gaptitudes[group]._title
-  else
-    return ""
   end
+  return ""
 end
 
--- return user aptitudes table
 function vRP.getUserAptitudes(user_id)
   local data = vRP.getUserDataTable(user_id)
-  if data ~= nil then
-    if data.gaptitudes == nil then
-      data.gaptitudes = {}
-    end
-
-    -- init missing aptitudes
-    for k,v in pairs(gaptitudes) do
-      if data.gaptitudes[k] == nil then -- init group
-        data.gaptitudes[k] = {}
-      end
-
-      local group = data.gaptitudes[k]
-      for l,w in pairs(v) do
-        if l ~= "_title" and group[l] == nil then -- init aptitude exp
-          group[l] = w[2] -- init exp
-        end
-      end
-    end
-
-    return data.gaptitudes
-  else
-    return nil
+  if data == nil then return nil end
+  if data.gaptitudes == nil then
+    data.gaptitudes = {}
   end
+  for k,v in pairs(gaptitudes) do
+    if data.gaptitudes[k] == nil then
+      data.gaptitudes[k] = {}
+    end
+    local group = data.gaptitudes[k]
+    for l,w in pairs(v) do
+      if l ~= "_title" and group[l] == nil then 
+        group[l] = w[2]
+      end
+    end
+  end
+  return data.gaptitudes
 end
 
 function vRP.varyExp(user_id, group, aptitude, amount)
   local def = vRP.getAptitudeDefinition(group, aptitude)
   local uaptitudes = vRP.getUserAptitudes(user_id)
   if def ~= nil and uaptitudes ~= nil then
-    -- apply variation
     local exp = uaptitudes[group][aptitude]
-    local level = math.floor(vRP.expToLevel(exp)) -- save level before variation
-
-    --- vary
+    local level = math.floor(vRP.expToLevel(exp))
     exp = exp+amount
-    --- clamp
     if exp < 0 then exp = 0 
     elseif def[3] >= 0 and exp > def[3] then exp = def[3] end
-
     uaptitudes[group][aptitude] = exp
-
-    -- info notify
     local player = vRP.getUserSource(user_id)
     if player ~= nil then
       local group_title = vRP.getAptitudeGroupTitle(group)
       local aptitude_title = def[1]
-
-      --- exp
       if amount < 0 then
         -- vRPclient._notify(player,lang.aptitude.lose_exp({group_title,aptitude_title,-1*amount}))
       elseif amount > 0 then
         -- vRPclient._notify(player,lang.aptitude.earn_exp({group_title,aptitude_title,amount}))
       end
+
       --- level up/down
       local new_level = math.floor(vRP.expToLevel(exp))
       local diff = new_level-level
@@ -128,14 +104,10 @@ end
 
 function vRP.getExp(user_id, group, aptitude)
   local uaptitudes = vRP.getUserAptitudes(user_id)
-  if uaptitudes ~= nil then
-    local vgroup = uaptitudes[group]
-    if vgroup ~= nil then
-      return vgroup[aptitude] or 0
-    end
-  end
-
-  return 0
+  if not uaptitudes then return 0 end 
+  local vgroup = uaptitudes[group]
+  if not vgroup then return end
+  return vgroup[aptitude] or 0
 end
 
 function vRP.setExp(user_id, group, aptitude, amount)
@@ -143,19 +115,14 @@ function vRP.setExp(user_id, group, aptitude, amount)
   vRP.varyExp(user_id, group, aptitude, amount-exp)
 end
 
--- return float
 function vRP.expToLevel(exp)
   return (math.sqrt(1+8*exp/exp_step)-1)/2
 end
 
--- return integer
 function vRP.levelToExp(lvl)
   return math.floor((exp_step*lvl*(lvl+1))/2)
 end
 
--- CONFIG
-
--- load config aptitudes
 for k,v in pairs(cfg.gaptitudes) do
   vRP.defAptitudeGroup(k,v._title or "")
   for l,w in pairs(v) do
