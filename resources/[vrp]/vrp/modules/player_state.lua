873f5b6180da85a6ae73126f75fab0d646e03cf5
Dione Batista <dione1993@hotmail.com>
2021-06-30 19:28:27 -0300

    Clear Player_State

--- resources/[vrp]/vrp/modules/player_state.lua
+++ resources/[vrp]/vrp/modules/player_state.lua
@@ -1,27 +1,23 @@
 local cfg = module("cfg/player_state")
 
--- client -> server events
 AddEventHandler("vRP:playerSpawn", function(user_id, source, first_spawn)
   local player = source
   local data = vRP.getUserDataTable(user_id)
   local tmpdata = vRP.getUserTmpTable(user_id)
-  if first_spawn then -- first spawn
-    -- cascade load customization then weapons
+  if first_spawn then
     if data.customization == nil then
       data.customization = cfg.default_customization
     end
-
-    if data.position then -- teleport to saved pos
+    if data.position then
       vRPclient.teleport(source,data.position.x,data.position.y,data.position.z)
     end
-
     if data.customization then
       vRPclient.setCustomization(source,data.customization) 
-      if data.weapons then -- load saved weapons
+      if data.weapons then
         vRPclient.giveWeapons(source,data.weapons,true)
-        if data.health then -- set health
+        if data.health then
           vRPclient.setHealth(source,data.health)
-          SetTimeout(5000, function() -- check coma, kill if in coma
+          SetTimeout(5000, function()
             if vRPclient.isInComa(player) then
               vRPclient.killComa(player)
             end
@@ -29,69 +25,54 @@ AddEventHandler("vRP:playerSpawn", function(user_id, source, first_spawn)
         end
       end
     else
-      if data.weapons then -- load saved weapons
+      if data.weapons then
         vRPclient.giveWeapons(source,data.weapons,true)
       end
-
       if data.health then
         vRPclient.setHealth(source,data.health)
       end
     end
-
-  else -- not first spawn (player died), don't load weapons, empty wallet, empty inventory
+  else
     vRP.setHunger(user_id,0)
     vRP.setThirst(user_id,0)
     vRP.clearInventory(user_id)
     vRP.setMoney(user_id,0)
-
-    -- load character customization
     if data.customization then
       vRPclient._setCustomization(source,data.customization)
     end
   end
-
   vRPclient._playerStateReady(source, true)
 end)
 
--- updates
-
-function tvRP.updatePos(x,y,z)
+function tvRP.updatePos()
   local user_id = vRP.getUserId(source)
-  if user_id then
-    local data = vRP.getUserDataTable(user_id)
-    local tmp = vRP.getUserTmpTable(user_id)
-    if data and (not tmp or not tmp.home_stype) then -- don't save position if inside home slot
-      data.position = {x = tonumber(x), y = tonumber(y), z = tonumber(z)}
-    end
-  end
+  if not user_id then return end
+  local data = vRP.getUserDataTable(user_id)
+  if not data then return end
+  local x,y,z = table.unpack(GetEntityCoords(GetPlayerPed(source)))
+  data.position = {x = tonumber(x), y = tonumber(y), z = tonumber(z)}
 end
 
 function tvRP.updateWeapons(weapons)
   local user_id = vRP.getUserId(source)
-  if user_id then
-    local data = vRP.getUserDataTable(user_id)
-    if data then
-      data.weapons = weapons
-    end
-  end
+  if not user_id then return end 
+  local data = vRP.getUserDataTable(user_id)
+  if not data then return end
+  data.weapons = weapons
 end
 
 function tvRP.updateCustomization(customization)
   local user_id = vRP.getUserId(source)
-  if user_id then
-    local data = vRP.getUserDataTable(user_id)
-    if data then
-      data.customization = customization
-    end
-  end
+  if not user_id then return end 
+  local data = vRP.getUserDataTable(user_id)
+  if not data then return end 
+  data.customization = customization
 end
 
 function tvRP.updateHealth(health)
   local user_id = vRP.getUserId(source)
-  if user_id then
-    local data = vRP.getUserDataTable(user_id)
-    if data then
-      data.health = health
-    end
-  end
+  if not user_id then return end
+  local data = vRP.getUserDataTable(user_id)
+  if not data then return end
+  data.health = health
 end
