8e9aedc92c724ec37b5f7e97a86f851a4c0a0007
Dione Batista <dione1993@hotmail.com>
2021-06-30 19:00:50 -0300

    Clear GUI Module

--- resources/[vrp]/vrp/modules/gui.lua
+++ resources/[vrp]/vrp/modules/gui.lua
@@ -2,52 +2,35 @@ local Tools = module("lib/Tools")
 
 local prompts = {}
 
--- prompt textual (and multiline) information from player
--- return entered text
 function vRP.prompt(source,title,default_text)
   local r = async()
   prompts[source] = r
-
   vRPclient._prompt(source, title,default_text)
-
   return r:wait()
 end
 
--- REQUEST
-
 local request_ids = Tools.newIDGenerator()
 local requests = {}
-
--- ask something to a player with a limited amount of time to answer (yes|no request)
--- time: request duration in seconds
--- return true (yes) or false (no)
 function vRP.request(source,text,time)
   local r = async()
-
   local id = request_ids:gen()
   local request = {source = source, cb_ok = r, done = false}
   requests[id] = request
-
-  vRPclient.request(source,id,text,time) -- send request to client
-
-  -- end request with a timeout if not already ended
+  vRPclient.request(source,id,text,time)
   SetTimeout(time*1000,function()
     if not request.done then
-      request.cb_ok(false) -- negative response
+      request.cb_ok(false)
       request_ids:free(id)
       requests[id] = nil
     end
   end)
-
   return r:wait()
 end
 
--- receive prompt result
 function tvRP.promptResult(text)
   if text == nil then
     text = ""
   end
-
   local prompt = prompts[source]
   if prompt ~= nil then
     prompts[source] = nil
@@ -55,12 +38,11 @@ function tvRP.promptResult(text)
   end
 end
 
--- receive request result
 function tvRP.requestResult(id,ok)
   local request = requests[id]
-  if request and request.source == source then -- end request
-    request.done = true -- set done, the timeout will not call the callback a second time
-    request.cb_ok(not not ok) -- callback
+  if request and request.source == source then 
+    request.done = true
+    request.cb_ok(not not ok)
     request_ids:free(id)
     requests[id] = nil
   end
