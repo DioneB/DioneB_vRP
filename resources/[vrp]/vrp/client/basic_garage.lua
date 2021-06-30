009d71cb82bb27464b82b1fd16645b9f5e0ec128
Dione Batista <dione1993@hotmail.com>
2021-06-30 19:42:35 -0300

    Clear Basic_Garage

--- resources/[vrp]/vrp/client/basic_garage.lua
+++ resources/[vrp]/vrp/client/basic_garage.lua
@@ -1,38 +1,25 @@
-
--- (experimental) this function return the nearest vehicle
--- (don't work with all vehicles, but aim to)
 function tvRP.getNearestVehicle(radius)
   local x,y,z = tvRP.getPosition()
   local ped = GetPlayerPed(-1)
   if IsPedSittingInAnyVehicle(ped) then
-    return GetVehiclePedIsIn(ped, true)
-  else
-    -- flags used:
-    --- 8192: boat
-    --- 4096: helicos
-    --- 4,2,1: cars (with police)
-
-    local veh = GetClosestVehicle(x+0.0001,y+0.0001,z+0.0001, radius+0.0001, 0, 8192+4096+4+2+1)  -- boats, helicos
-    if not IsEntityAVehicle(veh) then veh = GetClosestVehicle(x+0.0001,y+0.0001,z+0.0001, radius+0.0001, 0, 4+2+1) end -- cars
-    return veh
+    return GetVehiclePedIsIn(ped, true) 
   end
+  local veh = GetClosestVehicle(x+0.0001,y+0.0001,z+0.0001, radius+0.0001, 0, 8192+4096+4+2+1)  -- boats, helicos
+  if not IsEntityAVehicle(veh) then veh = GetClosestVehicle(x+0.0001,y+0.0001,z+0.0001, radius+0.0001, 0, 4+2+1) end -- cars
+  return veh
 end
 
 function tvRP.fixeNearestVehicle(radius)
   local veh = tvRP.getNearestVehicle(radius)
-  if IsEntityAVehicle(veh) then
-    SetVehicleFixed(veh)
-  end
+  if not IsEntityAVehicle(veh) then return end
+  SetVehicleFixed(veh)
 end
 
-
--- eject the ped from the vehicle
 function tvRP.ejectVehicle()
   local ped = GetPlayerPed(-1)
-  if IsPedSittingInAnyVehicle(ped) then
-    local veh = GetVehiclePedIsIn(ped,false)
-    TaskLeaveVehicle(ped, veh, 4160)
-  end
+  if not IsPedSittingInAnyVehicle(ped) then return end
+  local veh = GetVehiclePedIsIn(ped,false)
+  TaskLeaveVehicle(ped, veh, 4160)
 end
 
 function tvRP.isInVehicle()
