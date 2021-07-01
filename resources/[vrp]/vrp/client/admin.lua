d495f5f0918b0ffd2e89ad3a72976a366cc93159
Dione Batista <dione1993@hotmail.com>
2021-07-01 16:30:42 -0300

    TPWay

--- resources/[vrp]/vrp/client/admin.lua
+++ resources/[vrp]/vrp/client/admin.lua
@@ -39,17 +39,43 @@ function tvRP.toggleNoclip()
     SetEntityVisible(ped, true, false)
   end
 end
+
 function tvRP.teleportToWaypoint()
-  noclip = not noclip
-  MainThread()
-  local ped = GetPlayerPed(-1)
-  if noclip then
-    SetEntityInvincible(ped, true)
-    SetEntityVisible(ped, false, false)
-  else
-    SetEntityInvincible(ped, false)
-    SetEntityVisible(ped, true, false)
-  end
+	local ped = PlayerPedId()
+	local veh = GetVehiclePedIsUsing(ped)
+	if IsPedInAnyVehicle(ped) then
+		ped = veh
+    end
+	local waypointBlip = GetFirstBlipInfoId(8)
+	local x,y,z = table.unpack(Citizen.InvokeNative(0xFA7C7F0AADF25D09,waypointBlip,Citizen.ResultAsVector()))
+	local ground
+	local groundFound = false
+	local groundCheckHeights = { 0.0,50.0,100.0,150.0,200.0,250.0,300.0,350.0,400.0,450.0,500.0,550.0,600.0,650.0,700.0,750.0,800.0,850.0,900.0,950.0,1000.0,1050.0,1100.0 }
+	for i,height in ipairs(groundCheckHeights) do
+		SetEntityCoordsNoOffset(ped,x,y,height,0,0,1)
+		RequestCollisionAtCoord(x,y,z)
+		while not HasCollisionLoadedAroundEntity(ped) do
+			RequestCollisionAtCoord(x,y,z)
+			Citizen.Wait(1)
+		end
+		Citizen.Wait(20)
+		ground,z = GetGroundZFor_3dCoord(x,y,height)
+		if ground then
+			z = z + 1.0
+			groundFound = true
+			break;
+		end
+	end
+	if not groundFound then
+		z = 1200
+		GiveDelayedWeaponToPed(PlayerPedId(),0xFBAB5776,1,0)
+	end
+	RequestCollisionAtCoord(x,y,z)
+	while not HasCollisionLoadedAroundEntity(ped) do
+		RequestCollisionAtCoord(x,y,z)
+		Citizen.Wait(1)
+	end
+	SetEntityCoordsNoOffset(ped,x,y,z,0,0,1)
 end
 
 function tvRP.isNoclip()
