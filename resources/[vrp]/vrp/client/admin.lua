d1d10e96ddc991149ee4970fc64a8155c52675d9
Dione Batista <dione1993@hotmail.com>
2021-07-01 16:37:42 -0300

    Clear

--- resources/[vrp]/vrp/client/admin.lua
+++ resources/[vrp]/vrp/client/admin.lua
@@ -29,30 +29,29 @@ end
 
 function tvRP.spawnVehicle(name)
   local mhash = GetHashKey(name)
-	while not HasModelLoaded(mhash) do
-		RequestModel(mhash)
-		Citizen.Wait(10)
-	end
-	if HasModelLoaded(mhash) then
-		local ped = PlayerPedId()
-		local nveh = CreateVehicle(mhash,GetEntityCoords(ped),GetEntityHeading(ped),true,false)
-		NetworkRegisterEntityAsNetworked(nveh)
-		while not NetworkGetEntityIsNetworked(nveh) do
-			NetworkRegisterEntityAsNetworked(nveh)
-			Citizen.Wait(1)
-		end
-		SetVehicleOnGroundProperly(nveh)
-		SetVehicleAsNoLongerNeeded(nveh)
-		SetVehicleIsStolen(nveh,false)
-		SetPedIntoVehicle(ped,nveh,-1)
-		SetVehicleNeedsToBeHotwired(nveh,false)
-		SetEntityInvincible(nveh,false)
-		SetVehicleNumberPlateText(nveh,'TESTES')
-		Citizen.InvokeNative(0xAD738C3085FE7E11,nveh,true,true)
-		SetVehicleHasBeenOwnedByPlayer(nveh,true)
-		SetVehRadioStation(nveh,"OFF")
-		SetModelAsNoLongerNeeded(mhash)
-	end
+  while not HasModelLoaded(mhash) do
+    RequestModel(mhash)
+    Citizen.Wait(10)
+  end
+	if not HasModelLoaded(mhash) then return end
+  local ped = PlayerPedId()
+  local nveh = CreateVehicle(mhash,GetEntityCoords(ped),GetEntityHeading(ped),true,false)
+  NetworkRegisterEntityAsNetworked(nveh)
+  while not NetworkGetEntityIsNetworked(nveh) do
+    NetworkRegisterEntityAsNetworked(nveh)
+    Citizen.Wait(1)
+  end
+  SetVehicleOnGroundProperly(nveh)
+  SetVehicleAsNoLongerNeeded(nveh)
+  SetVehicleIsStolen(nveh,false)
+  SetPedIntoVehicle(ped,nveh,-1)
+  SetVehicleNeedsToBeHotwired(nveh,false)
+  SetEntityInvincible(nveh,false)
+  SetVehicleNumberPlateText(nveh,'TESTES')
+  Citizen.InvokeNative(0xAD738C3085FE7E11,nveh,true,true)
+  SetVehicleHasBeenOwnedByPlayer(nveh,true)
+  SetVehRadioStation(nveh,"OFF")
+  SetModelAsNoLongerNeeded(mhash)
 end 
 
 function tvRP.toggleNoclip()
